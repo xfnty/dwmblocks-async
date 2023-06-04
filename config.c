528cce29dc01f9cbf71a6891f8d0d2339c8c7a43
xfnty <tsentsevitsky.v@gmail.com>
2023-06-04 11:45:58 +0700

    Initial commit

---
+++ config.c
@@ -0,0 +1,11 @@
+#include "config.h"
+
+#include "block.h"
+#include "util.h"
+
+Block blocks[] = {
+    {"dwmblocks-memory",  10,   1 },
+    {"dwmblocks-date",    1,    2},
+};
+
+const unsigned short blockCount = LEN(blocks);
