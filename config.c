f8b7ee2929b1d81e1ae4d2d99717c029152c21dd
xfnty <tsentsevitsky.v@gmail.com>
2023-06-04 12:50:42 +0700

    Minor changes

--- config.c
+++ config.c
@@ -4,7 +4,7 @@
 #include "util.h"
 
 Block blocks[] = {
-    // {"dwmblocks-memory",  10,   1 },
+//    {"dwmblocks-memory",  10,   1 },
     {"dwmblocks-time",    1,    2},
     {"dwmblocks-battery",    60,    3},
 };
