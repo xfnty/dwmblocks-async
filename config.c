50286dad2a2c99ad54a023a7f865091d6c5165ca
xfnty <tsentsevitsky.v@gmail.com>
2023-06-08 11:57:21 +0700

    minor changes

--- config.c
+++ config.c
@@ -4,10 +4,10 @@
 #include "util.h"
 
 Block blocks[] = {
-//    {"dwmblocks-memory",  10,   1 },
-    {"dwmblocks-time",    1,    2},
-    {"dwmblocks-backlight",    3,    4},
-    {"dwmblocks-battery",    60,    3},
+    {"dwmblocks-memory",    3,      0},
+    {"dwmblocks-backlight", 0,      1},
+    {"dwmblocks-time",      1,      0},
+    {"dwmblocks-battery",   60,     0},
 };
 
 const unsigned short blockCount = LEN(blocks);
